acbd0c158370a20160ce3907b202fcd4f046c336
Tharinigovind <tharinigt97@gmail.com>
2019-06-13 16:32:46 +0530

    react sample

--- src/App.js
+++ src/App.js
@@ -1,26 +1,12 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React ,{Component} from 'react';
 
-function App() {
+class App extends Component {
+  render(){const
+     name=this.props.name,
+     age=this.props.age;
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
-
+    <div>
+      <h1>My name:{name}</h1>
+      <h2>My age:{age}</h2>
+    </div>);}}
 export default App;
